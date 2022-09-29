823b321a3d4bb5e44cb105776c57bd5be06595c7
Markus <markus.ydreskog@gmail.com>
2022-09-29 11:09:13 +0200

    Added ability to decrypt file from command line to easier port to new version

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ pub mod password;
 pub mod ui;
 
 use crate::password::PasswordEntries;
-use crate::password::{ModuleList, HOME_ENV};
+use crate::password::{decrypt_from_der, ModuleList, HOME_ENV};
 use crate::ui::{run_app, ModuleUI};
+use std::env;
 use std::io;
 
-use std::env;
 use std::fs::{self, File};
 use std::path::PathBuf;
 
@@ -21,6 +21,31 @@ use tui::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut arg_it = env::args();
+    arg_it.next();
+    while let Some(arg) = arg_it.next() {
+        match arg.as_ref() {
+            "-decrypt" => {
+                if let Some(file) = arg_it.next() {
+                    if let Some(der_file) = arg_it.next() {
+                        let mut pw = String::new();
+                        io::stdin().read_line(&mut pw)?;
+                        println!("Decrypting {} from {}", file, der_file);
+                        decrypt_from_der(pw.trim(), &file, &der_file)?;
+
+                        return Ok(());
+                    }
+                }
+                println!("No files given");
+                return Ok(());
+            }
+            _ => {
+                println!("Invalid argument");
+                return Ok(());
+            }
+        }
+    }
+
     let mut base_path = PathBuf::from(env::var(HOME_ENV)?);
     let mut file = env::var(HOME_ENV)?;
     base_path.push(".pwmanager");
